55bc3cab58cc1f2132cc44c44c91ad15b76b4dd6
sianida26 <sianida26@users.noreply.github.com>
2023-01-17 11:06:13 +0000

    deploy: e0f913547237a327577d1b2da7c769908a970b6d

--- assets/main-15958814.js
+++ assets/main-15958814.js
@@ -1,10 +1,10 @@
-import"./modulepreload-polyfill-ec808ebb.js";function n(e){let t=0;const o=r=>{t=r,e.innerHTML=`count is ${t}`};e.addEventListener("click",()=>o(t+1)),o(0)}document.querySelector("#app").innerHTML=`
+import"./modulepreload-polyfill-ec808ebb.js";const r="/opmx/assets/typescript-f6ead1af.svg";function c(e){let t=0;const o=s=>{t=s,e.innerHTML=`count is ${t}`};e.addEventListener("click",()=>o(t+1)),o(0)}document.querySelector("#app").innerHTML=`
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="/" class="logo vanilla" alt="TypeScript logo" />
+      <img src="${r}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
@@ -14,4 +14,4 @@ import"./modulepreload-polyfill-ec808ebb.js";function n(e){let t=0;const o=r=>{t
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`;n(document.querySelector("#counter"));
+`;c(document.querySelector("#counter"));
