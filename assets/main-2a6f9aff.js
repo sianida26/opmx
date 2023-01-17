00061497ce7ac7f0e18e36268e6a2ab098296a2c
sianida26 <sianida26@users.noreply.github.com>
2023-01-17 09:59:01 +0000

    deploy: 9fd33f109134929893ccfc113b9ac6e67857d4e1

---
+++ assets/main-2a6f9aff.js
@@ -0,0 +1,17 @@
+import"./modulepreload-polyfill-ec808ebb.js";function n(e){let t=0;const o=r=>{t=r,e.innerHTML=`count is ${t}`};e.addEventListener("click",()=>o(t+1)),o(0)}document.querySelector("#app").innerHTML=`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="/" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;n(document.querySelector("#counter"));
---
+++ assets/task1-0eb4fd2c.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-ec808ebb.js";window.addEventListener("DOMContentLoaded",()=>{alert("berhasil!"),document.getElementById("coba").innerHTML="Horee berhasil!"});
