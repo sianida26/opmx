6b58bb68b60a7035bb8ca2468f0cdabc64da5fed
sianida26 <sianida26@users.noreply.github.com>
2023-01-17 11:37:44 +0000

    deploy: c0237956e6ed5446000a936523ca6f6f0160e44e

---
+++ assets/s1t2-5d99c279.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-ec808ebb.js";window.addEventListener("DOMContentLoaded",()=>{document.querySelector("#coba").innerHTML="Will be implemented soon"});
--- assets/task1-8e2bbc28.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-ec808ebb.js";window.addEventListener("DOMContentLoaded",()=>{document.getElementById("coba").innerHTML="Will be implemented soon"});
