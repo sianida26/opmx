f087a2aa379dd161384dc58238b9043b435c9e10
sianida26 <chesakeselekkebo@gmail.com>
2023-01-17 19:10:50 +0700

    add image to task 2

--- session_1/task_2/index.ts
+++ session_1/task_2/index.ts
@@ -1,5 +1,30 @@
-window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#coba')!.innerHTML = "Will be implemented soon";
-});
+import * as PIXI from 'pixi.js'
+import image from './aJuU_1.png'
 
-export {}
+//create a container and appends to the html
+const app = new PIXI.Application({ background: '#1099bb' });
+document.body.appendChild(app.view as any);
+
+//create tree image as background
+const tree = PIXI.Sprite.from(image);
+app.stage.addChild(tree);
+
+//set image's size to be fit on container
+tree.width = app.screen.width
+tree.height = app.screen.height
+
+//input fields
+const inputs = Array.from(document.getElementsByClassName("s1t2_input") as HTMLCollectionOf<HTMLElement>)
+
+const positions = [
+    [100,100],
+    [0,0],
+    [0,0],
+]
+
+inputs.forEach((input, i) => {
+    input.style.position = "absolute"
+    input.style.display = "inline"
+    input.style.left = positions[i][0] + "px"
+    input.style.top = positions[i][1] + "px"
+})
