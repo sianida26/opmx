4bf4f249cf108e4bdf98557410cbae93a6d68904
sianida26 <chesakeselekkebo@gmail.com>
2023-01-17 15:55:28 +0700

    First commit

---
+++ task1/task1.ts
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    alert("berhasil!");
+    document.getElementById("coba")!.innerHTML = "Horee berhasil!";
+});
+
+export {}
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        task1: resolve(__dirname, 'task1/index.html'),
+      },
+    },
+  },
+  appType: 'mpa'
+})
